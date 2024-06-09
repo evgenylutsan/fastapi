025a728f202c944a21e87096c2c8173ac4e32341
evgenylutsan <evgeny_lutsan@mail.ru>
2024-06-10 01:03:43 +0300

    z

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
 FROM python:3.9
 
-RUN mkdir /app
-
 WORKDIR /app
 
-COPY requirements.txt .
+COPY requirements.txt /app/requirements.txt
 
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r /app/requirements.txt
 
-COPY . .
+COPY . /app
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
 
--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Qwerty1234@localhost:5432/Eventtoday_DB"
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:Qwerty1234@db:5432/Eventtoday_DB')
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
