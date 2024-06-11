c2c213ba493add703282f7b45418b3b331cacf37
evgenylutsan <evgeny_lutsan@mail.ru>
2024-06-11 07:07:07 +0300

    z

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
 import uvicorn
 from app.router import event, user, speaker, sponsor, calendar
 from app.db import models
@@ -6,6 +8,28 @@ from app.db.database import engine
 from app.auth import authentication
 
 app = FastAPI()
+
+security = HTTPBasic()
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = "admin"
+    correct_password = "admin"
+    if credentials.username != correct_username or credentials.password != correct_password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+@app.get("/docs", include_in_schema=False)
+async def get_docs(username: str = Depends(get_current_username)):
+    return get_swagger_ui_html(openapi_url=app.openapi_url, title="docs")
+
+@app.get("/redoc", include_in_schema=False)
+async def get_redoc(username: str = Depends(get_current_username)):
+    return get_redoc_html(openapi_url=app.openapi_url, title="redoc")
+
 app.include_router(authentication.router)
 app.include_router(event.router)
 app.include_router(user.router)
