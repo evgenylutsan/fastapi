d96eccbc2989a98a02810c72eb7ec22fd8b88e89
evgenylutsan <evgeny_lutsan@mail.ru>
2024-06-12 23:11:14 +0300

    z

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ from app.auth import authentication
 from starlette.requests import Request
 from starlette.responses import Response
 import base64
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
@@ -64,6 +66,18 @@ app.include_router(user.router)
 app.include_router(speaker.router)
 app.include_router(sponsor.router)
 app.include_router(calendar.router)
+
+origins = [
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(engine)
 
